4f747d07114a45923638004a2f2b8048b6d6752b
Gulrez Alam <egulrezalam@gmail.com>
2024-02-02 19:56:20 +0530

    Update Procfile and database settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ebeeafDfEB35Dafc33BF1c6bdEa6f5-G',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '18811',
     }
 }
 
