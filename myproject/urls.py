28fa8ffda10276a713840a1cd2f6e6f36da13986
Gulrez Alam <egulrezalam@gmail.com>
2024-02-03 01:26:00 +0530

    Add password reset functionality and improve UI

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,11 +19,26 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
     path('', RedirectView.as_view(url='myapp/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
